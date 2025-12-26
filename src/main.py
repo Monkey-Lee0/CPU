3980e2b589fcd1468787c952a12d16c224c52386
ckjsuperhh <846230484@qq.com>
2025-12-26 13:23:29 +0800

    trial commit

--- src/main.py
+++ src/main.py
@@ -2,58 +2,72 @@ from assassyn.frontend import *
 from assassyn.backend import elaborate
 from assassyn import utils
 import assassyn
-import os
-import sys
 
-class Subtractor(Module):
+import os
+from contextlib import contextmanager
+
+# 先定义上下文管理器，用于临时抑制输出
+@contextmanager
+def _suppress_output():
+    """内部辅助上下文管理器：临时屏蔽标准输出和标准错误"""
+    # 保存原始的 stdout 和 stderr，用于后续恢复
+    import sys
+    original_stdout = sys.stdout
+    original_stderr = sys.stderr
+
+    try:
+        # 打开系统空设备（自动适配 Windows/nul 和 Linux/Mac/dev/null）
+        with open(os.devnull, 'w') as null_device:
+            # 重定向标准输出和错误到空设备
+            sys.stdout = null_device
+            sys.stderr = null_device
+            yield  # 执行被包裹的代码块
+    finally:
+        # 无论是否发生异常，都恢复原始输出流
+        sys.stdout = original_stdout
+        sys.stderr = original_stderr
+
+def run_quietly(func, *args, **kwargs):
+    """
+    静默运行目标函数的封装函数
+    :param func: 待静默运行的目标函数
+    :param args: 目标函数的位置参数
+    :param kwargs: 目标函数的关键字参数
+    :return: 目标函数 func 的执行返回值
+    """
+    # 使用上下文管理器包裹目标函数的执行，实现静默运行
+    with _suppress_output():
+        # 传递参数并执行目标函数，保存返回值
+        result = func(*args, **kwargs)
+    # 返回目标函数的执行结果
+    return result
+
+class Adder(Module):
     def __init__(self):
-        super().__init__(ports={'sub_a': Port(Int(32)), 'sub_b': Port(Int(32))})
+        super().__init__(
+            ports={
+                'a':Port(Int(32)),
+                'b':Port(Int(32))
+            }
+        )
 
     @module.combinational
     def build(self):
-        a, b = self.pop_all_ports(False)
-        c = a - b
-        log("减法器: {} - {} = {}", a, b, c)
+        a, b = self.pop_all_ports(True)
+        c=a+b
+        log("{} + {} = {}", a, b, c)
 
-# LeftPath: 创建部分 Bind
-class LeftPath(Module):
-    def __init__(self):
-        super().__init__(ports={'lhs_a': Port(Int(32))})
-
-    @module.combinational
-    def build(self, sub: Subtractor):
-        lhs_a = self.pop_all_ports(True)
-        # 只绑定 sub_a，返回部分 Bind
-        return sub.bind(sub_a=lhs_a)
-
-# RightPath: 完成 Bind 并创建 AsyncCall
-class RightPath(Module):
-    def __init__(self):
-        super().__init__(ports={'rhs_b': Port(Int(32))})
-
-    @module.combinational
-    def build(self, bound_sub):
-        rhs_b = self.pop_all_ports(True)
-        # 添加 sub_b 并创建 AsyncCall
-        call = bound_sub.async_called(sub_b=rhs_b)
-        call.bind.set_fifo_depth(sub_a=2, sub_b=2)
-
-# Driver: 数据分流
 class Driver(Module):
     def __init__(self):
-        super().__init__(ports={})
+        super().__init__({})
 
     @module.combinational
-    def build(self, lhs: LeftPath, rhs: RightPath):
+    def build(self, adder: Adder):
         cnt = RegArray(Int(32), 1)
         (cnt & self)[0] <= cnt[0] + Int(32)(1)
-
-        v = cnt[0] * cnt[0]
-        call_lhs = lhs.async_called(lhs_a=v[0:31].bitcast(Int(32)))
-        call_lhs.bind.set_fifo_depth(lhs_a=2)
-
-        call_rhs = rhs.async_called(rhs_b=cnt[0])
-        call_rhs.bind.set_fifo_depth(rhs_b=2)
+        cond = cnt[0] < Int(32)(100)
+        with Condition(cond):
+            adder.async_called(a=cnt[0], b=cnt[0])
 
 if __name__ == "__main__":
     sys = SysBuilder('adder')
@@ -73,7 +87,13 @@ if __name__ == "__main__":
         random=True
     )
 
-    simulator_path, verilator_path = elaborate(sys, **config)
+    def run_elaborate():
+        return elaborate(sys, **config)
 
-    print(utils.run_simulator(simulator_path))
+    simulator_path, verilator_path = run_quietly(run_elaborate)
+
+    def run_simulator():
+        return utils.run_simulator(simulator_path)
+
+    print(run_quietly(run_simulator))
 
