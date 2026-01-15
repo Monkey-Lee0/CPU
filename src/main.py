c25d09162669b0a26ea312ff469912d918ce6794
ckjsuperhh <846230484@qq.com>
2026-01-15 22:34:46 +0800

    add test_basic2(totally dependency)

--- src/main.py
+++ src/main.py
@@ -33,12 +33,12 @@ def run_quietly(func, *args, **kwargs):
 
 if __name__ == "__main__":
 
-    sys = buildSys('test_basic')
+    sys = buildSys('test_basic3')
     print(sys)
 
     config = config(
         verilog=utils.has_verilator(),
-        sim_threshold=600,
+        sim_threshold=300,
         idle_threshold=200,
         resource_base=Path(__file__).resolve().parent.parent,
         random=True,
